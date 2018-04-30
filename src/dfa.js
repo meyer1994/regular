2ca607aedc702eb5f4d37594487e1c29405d288a
Joao Meyer <1994meyer@gmail.com>
2018-04-30 16:53:47 -0300

    Added skeleton of DFA class

---
+++ src/dfa.js
@@ -0,0 +1,20 @@
+
+class DFA {
+  constructor () {
+    throw new Error('TODO')
+  }
+
+  toRegExp () {
+    throw new Error('TODO')
+  }
+
+  toGrammar () {
+    throw new Error('TODO')
+  }
+
+  static minimize (dfa) {
+    throw new Error('TODO')
+  }
+}
+
+module.exports = DFA
