12d9a6eb89586994fd7c095efb1d66310f20dd4f
Joao Meyer <1994meyer@gmail.com>
2018-05-05 14:21:54 -0300

    Created determinism check.

--- src/nfa.js
+++ src/nfa.js
@@ -1,46 +1,46 @@
 
 class NFA {
-	/**
-	 * @brief Constructor
-	 *
-	 * The transition table will be in the following form:
-	 * Accepts 'aaa', 'bb' and 'c'.
-	 *   | a | b | c |
-	 * --|---|---|---|
-	 * S | A | B | C |
-	 * --|---|---|---|
-	 * A | B | B | B |
-	 * --|---|---|---|
-	 * B | C | C | C |
-	 * --|---|---|---|
-	 * C | - | - | - |
-	 *
-	 * As an object:
-	 * {
-	 * 	'S' : { 'a': [ 'A' ], 'b': [ 'B' ], 'c': [ 'C' ] }
-	 * 	'A' : { 'a': [ 'B' ], 'b': [ 'B' ], 'c': [ 'B' ] }
-	 * 	'B' : { 'a': [ 'C' ], 'b': [ 'C' ], 'c': [ 'C' ] }
-	 * 	'C' : { 'a': [], 'b': [], 'c': [] }
-	 * }
-	 *
-	 * Both representations represent the exact same thing.
-	 *
-	 * @param {String} start  String representing the starting state.
-	 * @param {Array}  accept List of string representing accepting states.
-	 * @param {Object} table  Object representing the table of states transitions.
-	 */
+  /**
+   * @brief Constructor
+   *
+   * The transition table will be in the following form:
+   * Accepts 'aaa', 'bb' and 'c'.
+   *   | a | b | c |
+   * --|---|---|---|
+   * S | A | B | C |
+   * --|---|---|---|
+   * A | B | B | B |
+   * --|---|---|---|
+   * B | C | C | C |
+   * --|---|---|---|
+   * C | - | - | - |
+   *
+   * As an object:
+   * {
+   *  'S' : { 'a': [ 'A' ], 'b': [ 'B' ], 'c': [ 'C' ] }
+   *  'A' : { 'a': [ 'B' ], 'b': [ 'B' ], 'c': [ 'B' ] }
+   *  'B' : { 'a': [ 'C' ], 'b': [ 'C' ], 'c': [ 'C' ] }
+   *  'C' : { 'a': [], 'b': [], 'c': [] }
+   * }
+   *
+   * Both representations represent the exact same thing.
+   *
+   * @param {String} start  String representing the starting state.
+   * @param {Array}  accept List of string representing accepting states.
+   * @param {Object} table  Object representing the table of states transitions.
+   */
   constructor (start, accept, table) {
     this.start = start
     this.accept = accept
-  	this.table = table
+    this.table = table
     this.alphabet = new Set()
 
     // Gets alphabet
     for (let state in table) {
-    	const row = table[state]
-    	for (let alpha in row) {
-    		this.alphabet.add(alpha)
-    	}
+      const row = table[state]
+      for (let alpha in row) {
+        this.alphabet.add(alpha)
+      }
     }
   }
 
@@ -49,6 +49,53 @@ class NFA {
   }
 
   toGrammar () {
+    throw new Error('TODO')
+  }
+
+  /**
+   * @brief Checks if the NFA is deterministic or not.
+   *
+   * An FA is deterministic if there is only one possible transition from one
+   * state to another by each character.
+   *
+   * Example:
+   *   |   a  |
+   * --|------|
+   * S | A, B |
+   * --|------|
+   * A |   B  |
+   * --|------|
+   * B |   C  |
+   * Is not deterministic.
+   *
+   * But this one is:
+   *   | a |
+   * --|---|
+   * S | A |
+   * --|---|
+   * A | B |
+   * --|---|
+   * B | C |
+   *
+   *
+   * @return {Boolean} True if deterministic, false otherwise.
+   */
+  isDeterministic () {
+    // For every state
+    for (let state in this.table) {
+      const row = this.table[state]
+      // Check if there is more than one possible transition per character
+      for (let alpha in row) {
+        const destiny = row[alpha]
+        if (destiny.length > 1) {
+          return false
+        }
+      }
+    }
+    return true
+  }
+
+  static determinize (dfa) {
     throw new Error('TODO')
   }
 
--- test/test_nfa.js
+++ test/test_nfa.js
@@ -2,19 +2,45 @@ const assert = require('assert')
 const NFA = require('../src/nfa')
 
 describe('NFA', function () {
-	describe('#constructor', function () {
-		it('Should separate the alphabet passed', function () {
-			const start = 'S'
-			const accept = [ 'A' ]
-			const table = {
-				'S': { 'a': [ 'S', 'A' ], 'b': [ 'A' ], 'c': [ 'C' ] },
-				'A': { 'a': [ 'S', 'A' ], 'b': [ 'C' ], 'c': [ 'A' ] },
-				'C': { 'a': [ 'S', 'A' ], 'b': [ 'C' ], 'c': [ 'S' ] },
-			}
-			const alpha = new Set([ 'a', 'b', 'c' ])
+  describe('#constructor', function () {
+    it('Should separate the alphabet passed', function () {
+      const start = 'S'
+      const accept = [ 'A' ]
+      const table = {
+        'S': { 'a': [ 'S', 'A' ], 'b': [ 'A' ], 'c': [ 'C' ] },
+        'A': { 'a': [ 'S', 'A' ], 'b': [ 'C' ], 'c': [ 'A' ] },
+        'C': { 'a': [ 'S', 'A' ], 'b': [ 'C' ], 'c': [ 'S' ] },
+      }
+      const alpha = new Set([ 'a', 'b', 'c' ])
 
-			const dfa = new NFA(start, accept, table)
-			assert.deepStrictEqual(dfa.alphabet, alpha)
-		})
-	})
+      const dfa = new NFA(start, accept, table)
+      assert.deepStrictEqual(dfa.alphabet, alpha)
+    })
+  })
+
+  describe('#isDeterministic', function () {
+    it('Should return true if deterministic', function () {
+      const start = 'S'
+      const accept = [ 'A' ]
+      const table = {
+        'S': { 'a': [ 'S' ] },
+        'A': { 'a': [ 'A' ] }
+      }
+
+      const nfa = new NFA(start, accept, table)
+      assert(nfa.isDeterministic())
+    })
+
+    it('Should return false if non-deterministic', function () {
+      const start = 'S'
+      const accept = [ 'A' ]
+      const table = {
+        'S': { 'a': [ 'S', 'A' ] },
+        'A': { 'a': [ 'A' ] }
+      }
+
+      const nfa = new NFA(start, accept, table)
+      assert(!nfa.isDeterministic())
+    })
+  })
 })
