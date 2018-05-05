e0629b048bdf47af7b1666d9b437b2993f366658
Joao Meyer <1994meyer@gmail.com>
2018-05-05 14:03:29 -0300

    Changed focus of development to NFA.
    - Removed DFA class. All DFAs are also NFAs;
    - Changed branch names;
    - Added basic constructor test.

--- src/grammar.js
+++ src/grammar.js
@@ -21,76 +21,29 @@ class Grammar {
    *
    * @param  {Object} obj Object representing the regular grammar.
    */
-  constructor (first, obj) {
-    this.checkFormat(first, obj)
-  }
-
-  checkFormat (first, obj) {
-    const left = new Set(Object.keys(obj))
-
-    // String
-    if (typeof first !== 'string') {
-      throw new Error('first should be a string')
-    }
-
-    // Object
-    // copied from here, added De Morgan law to logic expression:
-    // https://stackoverflow.com/a/16608074/5092038
-    if ((!obj) || (obj.constructor !== Object)) {
-      throw new Error('obj should be an object ({})')
-    }
-
-    // Single uppercase letter
-    if (first.match(/^[A-Z]$/) === null) {
-      throw new Error('first should be a single uppercase letter')
-    }
-
-    // On left side
-    if (!left.has(first)) {
-      throw new Error('first should appear in left side')
-    }
-
-    // Left side
-    for (let item of left) {
-      // Length
-      if (item.length !== 1) {
-        throw new Error('length of left side should always be 1')
-      }
-
-      // Uppercase
-      if (item.match(/^[A-Z]$/) === null) {
-        throw new Error('left side should be a single uppercase letter')
-      }
-    }
-
-    // Right side
-    // First pdocution
-    // Recursive and has epslon
-    if (this.epslonRecursive(first, obj[first])) {
-      throw new Error('first production should not have epslon if recursive')
-    }
-  }
-
-  epslonRecursive (left, right) {
-    return this.isRecursive(left, right) && right.indexOf('&') !== -1
-  }
-
-  isRecursive (left, right) {
-    for (let item of right) {
-      if (item.length === 2 && item[1] === left) {
-        return true
-      }
-    }
-    return false
+   constructor (first, productions) {
+    this.first = first
+    this.productions = productions
   }
 
   /**
-   * Converts this regular grammar into a deterministic finite automata
+   * Converts this regular grammar into a deterministic finite automaton.
    *
-   * @return {DFA} This regular grammar, converted to an automata.
+   * @return {DFA} This regular grammar, converted to an automaton.
    */
   toDFA () {
     throw new Error('TODO')
+  }
+
+  /**
+   * Generates a grammar from a non-deterministic finite automaton.
+   *
+   * @param {NFA} nfa
+   *
+   * @return New Grammar object that represents
+   */
+  static fromNFA (nfa) {
+
   }
 
   /**
@@ -111,36 +64,56 @@ class Grammar {
    * @param  {Grammar} rga Regular grammar A.
    * @param  {Grammar} rgb Regular grammar B.
    *
-   * @return {Grammar}     Regular grammar that is A concat B.
+   * @return {Grammar}     Regular grammar that is A concatenated with B.
    */
   static concat (rga, rgb) {
     throw new Error('TODO')
   }
 
   /**
-   * Gets the closure representation of the grammar.
+   * Gets the regular grammar that represents the closure of rga.
    *
-   * @param  {Grammar} rga A grammar.
-   * @return {Grammar}     Closure of grammar A.
+   * @param  {Grammar} rga Regular grammar.
+   *
+   * @return {Grammar}     Regular grammar that is the closure of A.
    */
   static closure (rga) {
     throw new Error('TODO')
   }
 
+  /**
+   * Gets the FA that represents the intersection of rga and rgb.
+   *
+   * @param  {Grammar} rga Regular grammar A.
+   * @param  {Grammar} rgb Regular grammar B.
+   *
+   * @return {Grammar}     Regular grammar that is A intersected with B.
+   */
   static intersection (rga, rgb) {
     throw new Error('TODO')
   }
 
+  /**
+   * Gets the FA that represents the difference of rga and rgb.
+   *
+   * @param  {Grammar} rga Regular grammar A.
+   * @param  {Grammar} rgb Regular grammar B.
+   *
+   * @return {Grammar}     Regular grammar that is A - B.
+   */
   static diff (rga, rgb) {
     throw new Error('TODO')
   }
 
-  static reverse (rga, rgb) {
+  /**
+   * Gets the FA that represents the reverse of rga.
+   *
+   * @param  {Grammar} rga Regular grammar A.
+   *
+   * @return {Grammar}     Regular grammar that is the reverse of A.
+   */
+  static reverse (rga) {
     throw new Error('TODO')
-  }
-
-  isRegular () {
-    
   }
 }
 
