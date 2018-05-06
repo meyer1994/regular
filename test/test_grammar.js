c01810a614ee6eb7edacd57262394246c8dc6421
Geovani da Silva Júnior <geovanij3@gmail.com>
2018-05-06 15:41:17 -0300

    Added fromNFA function && tests

--- test/test_grammar.js
+++ test/test_grammar.js
@@ -1,6 +1,40 @@
-// const assert = require('assert')
-// const Grammar = require('../src/grammar')
+const assert = require('assert')
+const NFA = require('../src/nfa')
+const Grammar = require('../src/grammar')
 
 describe('Grammar', function () {
   describe('#constructor()', function () {})
+  describe('#fromNFA', function () {
+    it('Should transform a NFA into a regular grammar', function () {
+      // language = odd number of a's
+      const start = 'S'
+      const accept = [ 'A' ]
+      const table = {
+        'S': { 'a': [ 'A' ] },
+        'A': { 'a': [ 'S' ] }
+      }
+      const dfa = new NFA(start, accept, table)
+      const productions = {
+        'S': [ 'aA', 'a' ],
+        'A': [ 'aS' ]
+      }
+      assert.deepStrictEqual(Grammar.fromNFA(dfa), new Grammar(start, productions))
+    })
+    it('Should add epsilon if NFA accepts it', function () {
+      // language = even number of a's
+      const start = 'S'
+      const accept = [ 'S' ]
+      const table = {
+        'S': { 'a': [ 'A' ] },
+        'A': { 'a': [ 'S' ] }
+      }
+      const dfa = new NFA(start, accept, table)
+      const productions = {
+        "S'": [ 'aA', '&' ],
+        'S': [ 'aA' ],
+        'A': [ 'aS', 'a' ]
+      }
+      assert.deepStrictEqual(Grammar.fromNFA(dfa), new Grammar("S'", productions))
+    })
+  })
 })
